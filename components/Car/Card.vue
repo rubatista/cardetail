d5f59d981cec90c301b9a3fe3b448108e5ab6bbb
rubatista <inforubenbaptista@gmail.com>
2023-05-03 18:15:32 +0100

    add localStorage module and use to fill hearts when i favorite items

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,20 +1,27 @@
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
 const props = defineProps({
   car: Object,
-})
+  favored: Boolean
+});
+
+const emit = defineEmits(['favor']);
 </script>
 
 <template>
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
-    <div class="flex h-full">
-      <nuxt-img
-        :src="car.url"
-        :alt="car.name"
-        class="w-[300px] h-full"
-      />
+    <img
+      :src="favored ? heartFilled : heartOutline"
+      @click="emit('favor', car.id)"
+      class="absolute w-5 right-5 top-2 z-20"
+      alt=""
+    />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <nuxt-img :src="car.url" :alt="car.name" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
