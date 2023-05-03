58a02867cf646c7f32de94a7317597a80b18af9b
rubatista <inforubenbaptista@gmail.com>
2023-05-03 12:32:38 +0100

    use useError to dynamically change error code and message

--- error.vue
+++ error.vue
@@ -1,4 +1,6 @@
 <script setup>
+const error = useError()
+
 const handleError = () => {
     navigateTo("/")
 }
@@ -6,8 +8,8 @@ const handleError = () => {
 
 <template>
     <div class="flex h-screen justify-center items-center flex-col">
-        <h1 class="text-9xl">404</h1>
-        <p class="mt-7 text-4xl">Page not found</p>
+        <h1 class="text-9xl">{{ error.statusCode }}</h1>
+        <p class="mt-7 text-4xl">{{ error.message }}</p>
         <button @click="handleError()" class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white">
             Go Back
         </button>
