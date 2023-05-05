8694852e181316eb18ace426b644270cd9bc8815
rubatista <inforubenbaptista@gmail.com>
2023-05-05 11:19:43 +0100

    adding car make filter

--- components/Car/Sidebar.vue
+++ components/Car/Sidebar.vue
@@ -1,4 +1,6 @@
 <script setup>
+const { makes } = useCars();
+
 const modal = ref({
   make: false,
   location: false,
@@ -9,19 +11,24 @@ const route = useRoute();
 
 const updateModal = (key) => {
   modal.value[key] = !modal.value[key];
-}
+};
 
 const onChangeLocation = () => {
-  if(!city.value) return;
-  if(!isNaN(parseInt(city.value))){
+  if (!city.value) return;
+  if (!isNaN(parseInt(city.value))) {
     throw createError({
       statusCode: 400,
-      message: "Invalid city name"
-    })
+      message: "Invalid city name",
+    });
   }
-  updateModal('location');
+  updateModal("location");
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = "";
+};
+
+const onChangeMake = (make) => {
+  updateModal("make")
+  navigateTo(`/city/${route.params.city}/car/${make}`)
 }
 </script>
 
@@ -29,13 +36,18 @@ const onChangeLocation = () => {
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">
+        {{ route.params.city }}
+      </h3>
       <div
         v-if="modal.location"
         class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
       >
         <input v-model="city" type="text" class="border p-1 rounded" />
-        <button @click="onChangeLocation()" class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+        <button
+          @click="onChangeLocation()"
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+        >
           Apply
         </button>
       </div>
@@ -43,7 +55,16 @@ const onChangeLocation = () => {
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{ route.params.make || "Any" }}
+      </h3>
+
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+        v-if="modal.make"
+      >
+        <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">{{ make }}</h4>
+      </div>
     </div>
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
