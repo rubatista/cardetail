879f9438619838faa1a69d205bfd5d2a357e08ea
rubatista <inforubenbaptista@gmail.com>
2023-05-05 12:16:12 +0100

    adding the car price filter

--- components/Car/Sidebar.vue
+++ components/Car/Sidebar.vue
@@ -7,7 +7,28 @@ const modal = ref({
   price: false,
 });
 const city = ref("");
+const priceRange = ref({
+  min: "",
+  max: ""
+})
 const route = useRoute();
+const router = useRouter();
+
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+
+  if(!minPrice && !maxPrice) return "Any";
+  else if (!minPrice && maxPrice){
+    return `< ${maxPrice}€`
+  }
+  else if(minPrice && !maxPrice){
+    return `> ${minPrice}€`
+  }
+  else{
+    return `${minPrice}€ - ${maxPrice}€`
+  }
+})
 
 const updateModal = (key) => {
   modal.value[key] = !modal.value[key];
@@ -29,6 +50,20 @@ const onChangeLocation = () => {
 const onChangeMake = (make) => {
   updateModal("make")
   navigateTo(`/city/${route.params.city}/car/${make}`)
+}
+
+const onChangePrice = () => {
+  updateModal('price');
+  if(priceRange.value.max && priceRange.value.min){
+    if(priceRange.value.min > priceRange.value.max) return;
+  }
+
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max
+    }
+  })
 }
 </script>
 
@@ -69,7 +104,13 @@ const onChangeMake = (make) => {
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize"></h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">{{ priceRangeText }}</h3>
+
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.price">
+        <input class="border p-1 rounded" type="number" placeholder="Min" v-model="priceRange.min" />
+        <input class="border p-1 rounded" type="number" placeholder="Max" v-model="priceRange.max" />
+        <button @click="onChangePrice" class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+      </div>
     </div>
   </div>
 </template>
