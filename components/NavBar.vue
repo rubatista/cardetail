1fc82aaf459fd644b8873f15ea8faa700feae03c
rubatista <inforubenbaptista@gmail.com>
2023-05-11 17:06:18 +0100

    adding login and logout to navbar and added a global middleware

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,29 @@
 <script setup>
 const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+const logout = async () => {
+
+    const {error} = await supabase.auth.signOut();
+
+    try {
+        await $fetch('/api/_supabase/session', {
+            method: "POST",
+            body: {event: "SIGNED_OUT", session: null}
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    if(error){
+        console.log(error)
+    }
+
+    user.value = null;
+
+    navigateTo('/');
+
+}
 </script>
 
 <template>    
@@ -7,7 +31,7 @@ const user = useSupabaseUser()
         <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
         <div v-if="user" class="flex">
             <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
-            <p>Logout</p>
+            <p class="cursor-pointer" @click="logout">Logout</p>
         </div>
         <NuxtLink to="/login" v-else>Login</NuxtLink>
     </header>
