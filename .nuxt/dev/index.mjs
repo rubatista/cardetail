02d56f363b081b9f666283143e1dc401cf959f2f
rubatista <inforubenbaptista@gmail.com>
2023-05-11 17:46:19 +0100

    start adding endpoints

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -547,9 +547,11 @@ const _LPkmIv = defineEventHandler(async (event) => {
   return "auth cookie set";
 });
 
+const _lazy_wKPcz9 = () => Promise.resolve().then(function () { return _city_$1; });
 const _lazy_e0LjtH = () => Promise.resolve().then(function () { return renderer$1; });
 
 const handlers = [
+  { route: '/api/cars/:city', handler: _lazy_wKPcz9, lazy: true, middleware: false, method: undefined },
   { route: '/__nuxt_error', handler: _lazy_e0LjtH, lazy: true, middleware: false, method: undefined },
   { route: '/api/_supabase/session', handler: _LPkmIv, lazy: false, middleware: false, method: undefined },
   { route: '/**', handler: _lazy_e0LjtH, lazy: true, middleware: false, method: undefined }
@@ -676,6 +678,166 @@ const template = _template;
 const errorDev = /*#__PURE__*/Object.freeze({
       __proto__: null,
       template: template
+});
+
+const cars = [
+	{
+		id: 1,
+		year: 2018,
+		name: "Range Rover Sport",
+		price: 84777,
+		city: "Toronto",
+		make: "Land Rover",
+		url: "https://www.carhelpcanada.com/wp-content/uploads/2019/12/2020-Range-Rover-Evoque-2.jpg",
+		seats: 5,
+		miles: "14,666",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et laboriosam!"
+	},
+	{
+		id: 2,
+		year: 2021,
+		name: "Rolls Royce Ghost",
+		price: 455000,
+		url: "https://robbreport.com/wp-content/uploads/2021/03/1-5.jpg?w=1000",
+		seats: 5,
+		city: "Miami",
+		make: "Rolls-Royce",
+		miles: "53,666",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et laboriosam! Numquam ut rem, blanditiis est rerum tenetur maxime delectus"
+	},
+	{
+		id: 4,
+		year: 2020,
+		name: "Porsche Taycan",
+		price: 180434,
+		url: "https://www.topgear.com/sites/default/files/cars-car/carousel/2021/02/pcgb20_1441_fine.jpg",
+		seats: 5,
+		make: "Porsche",
+		city: "Toronto",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	},
+	{
+		id: 5,
+		name: "Land Rover Luxury",
+		price: 200434,
+		url: "https://www.motortrend.com/uploads/2022/04/2022-Range-Rover-First-Edition-P530-SWB-on-road-1.jpg?fit=around%7C875:492",
+		seats: 5,
+		make: "Land Rover",
+		city: "Toronto",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	},
+	{
+		id: 6,
+		name: "Toyota Corolla",
+		year: 2015,
+		price: 23000,
+		url: "https://crdms.images.consumerreports.org/c_lfill,w_470,q_auto,f_auto/prod/cars/chrome/white/2022TOC040001_1280_01",
+		seats: 5,
+		make: "Toyota",
+		city: "Toronto",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	},
+	{
+		id: 7,
+		name: "Toyota Sienna",
+		year: 2019,
+		price: 34000,
+		url: "https://upload.wikimedia.org/wikipedia/commons/2/22/2021_Toyota_Sienna_XLE_Hybrid%2C_front_12.21.21.jpg",
+		seats: 5,
+		make: "Toyota",
+		city: "Toronto",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	},
+	{
+		id: 8,
+		name: "Porsche Targa",
+		year: 2014,
+		price: 250000,
+		url: "https://hips.hearstapps.com/hmg-prod/images/2021-porsche-911-targa-4-4s-108-1597085217.jpg?crop=0.521xw:0.389xh;0.274xw,0.413xh&resize=1200:*",
+		seats: 5,
+		make: "Porsche",
+		city: "Toronto",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	},
+	{
+		id: 9,
+		name: "Kia Seltos",
+		year: 2012,
+		price: 39000,
+		url: "https://i.gaw.to/content/photos/53/04/530444-kia-seltos-2024-une-bonne-mise-a-jour-s-en-vient.jpg?1024x640",
+		seats: 5,
+		make: "Kia",
+		city: "Miami",
+		miles: "0",
+		features: [
+			"No Accidents",
+			"Low KM",
+			"Vehicle Detailed",
+			"Leather Interior"
+		],
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio et"
+	}
+];
+
+const _city_ = defineEventHandler((event) => {
+  const { city } = event.context.params;
+  let filteredCars = cars;
+  filteredCars = filteredCars.filter((car) => {
+    return car.city.toLowerCase() === city.toLowerCase();
+  });
+  return filteredCars;
+});
+
+const _city_$1 = /*#__PURE__*/Object.freeze({
+      __proto__: null,
+      default: _city_
 });
 
 const appRootId = "__nuxt";
